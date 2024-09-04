a09f790e9e020dc23ffe37b50aaa8bf5207e8b56
Paulo <pr.tech82@hotmail.com>
2024-09-03 22:26:38 -0300

    03/09

--- script.js
+++ script.js
@@ -44,6 +44,36 @@ function toggleSom() {
   }
 }
 
-function onoff(){
-  document.getElementById('onoff').click
+function onOff() {
+  // Get the button element
+  const button = document.getElementById('onoff');
+  const inputs = document.getElementsByClassName('ipt'); 
+  
+  // Toggle the button's text and color
+  if (button.innerText === 'ON') {
+    button.style.color = 'red';
+    button.innerText = 'OFF';
+    playSound = function() {};
+    document.getElementById('resultado').value= ''
+
+    // Change the color of the input elements to black
+    Array.prototype.forEach.call(inputs, function(input) {
+      input.style.background = 'rgb(98, 95, 95)';
+      input.style.color = 'rgb(98, 95, 95)';
+      input.style.setProperty('--placeholder-color', '');
+    });
+  
+  } else {
+    button.style.color = 'rgba(55, 232, 61)'; // reset to default color
+    button.innerText = 'ON';
+    playSound = playSoundOriginal;
+    document.getElementById('resultado').value= ''
+    
+    // Reset the color of the input elements to default
+    Array.prototype.forEach.call(inputs, function(input) {
+      input.style.background = 'rgb(98, 95, 95)'; // or set it to a default color
+      input.style.color = 'rgba(55, 232, 61, 0.859)';
+      input.style.setProperty('--placeholder-color', 'rgba(55, 232, 61, 0.859)');
+    });
+  }
 }
